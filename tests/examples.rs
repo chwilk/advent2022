ed0c40acc593cf2923a97fdd0705a3c3695c06c8
chwilk <chwilk@gmail.com>
2022-11-29 17:51:26 -0600

    Add framework for testing before advent begins

---
+++ tests/examples.rs
@@ -0,0 +1,90 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+use std::error::Error;
+use std::fs;
+
+// Borrowed from command line rust
+type TestResult = Result<(), Box<dyn Error>>;
+
+struct TestDay {
+    day: String,
+    args: Vec<String>,
+    half: String,
+    bin: Option<String>,
+}
+
+// Day 1
+#[test]
+fn day1a() -> TestResult {
+    TestDay::day("1").half("a").test_file().assert()
+}
+// Day 2
+/*
+#[test]
+fn day2a() -> TestResult {
+    TestDay::day("2").half("a").test_file().assert()
+}
+*/
+// Day 21 (example with arguments)
+/*
+#[test]
+fn day21() -> TestResult {
+    TestDay::day("21")
+        .half("")
+        .arg("4")
+        .arg("8")
+        .assert()
+}
+*/
+
+impl TestDay {
+    pub fn day(day: &str) -> TestDay {
+        TestDay {
+            day: day.to_string(),
+            args: Vec::new(),
+            half: String::from(""),
+            bin: None,
+        }
+    }
+
+    // add argument
+    pub fn arg<'a>(&'a mut self, arg: &str) -> &'a mut TestDay {
+        self.args.push(arg.to_string());
+        self
+    }
+
+    // add test filename (optional)
+    pub fn test_file<'a>(&'a mut self) -> &'a mut TestDay {
+        self.args.push(format!("tests/inputs/test{}.dat", self.day));
+        self
+    }
+
+    // add a,b half of day designator (optional)
+    pub fn half<'a>(&'a mut self, half: &str) -> &'a mut TestDay {
+        self.half.push_str(half);
+        self
+    }
+
+    // set binary (default advent{day}{half}) (optional)
+    pub fn set_bin<'a>(&'a mut self, bin: &str) -> &'a mut TestDay {
+        self.bin = Some(bin.to_string());
+        self
+    }
+
+    // Do the test run
+    pub fn assert<'a>(&'a mut self) -> TestResult {
+        let expected = fs::read_to_string(format!("tests/outputs/test{}{}.out", self.day, self.half))?;
+
+        let bin = match &self.bin {
+            None    => format!("day{}{}", self.day, self.half),
+            Some(b) => b.to_string(),
+        };
+
+        Command::cargo_bin(bin)?
+            .args(&self.args)
+            .assert()
+            .stdout(predicate::str::contains(expected.trim_end()));
+
+        Ok(())
+    }
+}
