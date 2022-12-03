d0e8707785142602624b907341213b6b29214351
chwilk <chwilk@gmail.com>
2022-12-03 00:50:12 -0600

    Updated to separate out part a and b solns

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -6,11 +6,17 @@ fn main() {
 
     let filename = find_filename(env::args());
 
-    let items: Vec<u32> = fs::read_to_string(filename)
-        .unwrap_or_else(|error| {
-            panic!("Could not read file: {:?}", error);
-        })
-        .lines()
+    let input = fs::read_to_string(filename)
+    .unwrap_or_else(|error| {
+        panic!("Could not read file: {:?}", error);
+    });
+
+    println! ("{}", parta(&input));
+    println! ("{}", partb(&input));
+}
+
+fn parta (input: &String) -> u32 {
+    let items: Vec<u32> = input.lines()
         .map(|sack| {
             let (a, b) = sack.split_at(sack.len()/2);
             let mut dupe: char = 'a';
@@ -30,6 +36,30 @@ fn main() {
         })
         .collect();
 
-    println! ("{:?}", items);
-    println! ("{:?}", items.iter().sum::<u32>());
+    items.iter().sum::<u32>()
+}
+
+fn partb (input: &String) -> u32 {
+    let sacks: Vec<u32> = input.lines()
+        .collect::<Vec<_>>()
+        .chunks(3)
+        .map(|team| {
+            let mut badge: char = 'a';
+            for item in team[0].chars() {
+                if team[1].contains(item) && team[2].contains(item) {
+                    badge = item;
+                    break;
+                }
+            }
+            badge
+        })
+        .map(|item| {
+            match item {
+                'A'..='Z' => item as u32 - 38,
+                _ => item as u32 - 96,
+            }
+        })
+        .collect();
+
+    sacks.iter().sum::<u32>()
 }
