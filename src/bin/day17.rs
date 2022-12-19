53e61e485bea132be9ddb19640ef130680fa22f0
chwilk <chwilk@gmail.com>
2022-12-19 09:39:31 -0600

    Got part 2, left in some warts to show failed ideas

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 use advent2022::find_filename;
@@ -34,9 +35,10 @@ fn main() {
         })
         .collect();
     println!("{}", play(jets.clone(), 2022));
+    println!("{}", play(jets.clone(), 1000000000000));
 }
 
-fn play (jets: Vec<Move>, turns: usize) -> usize {
+fn play (jets: Vec<Move>, turns: u64) -> u64 {
     use crate::Move::{L,R,D};
     use crate::Rock::{*};
 
@@ -44,8 +46,11 @@ fn play (jets: Vec<Move>, turns: usize) -> usize {
     chamber.push(0xff as u8);
 
     let mut jet: usize = 0;
+    let mut height: u64 = 0;
+    let mut cycles = HashMap::new();
 
-    for turn in 0..turns {
+    let mut turn = 0;
+    while turn < turns {
         let rock = match turn % 5 {
             0 => Horiz,
             1 => Plus,
@@ -55,6 +60,8 @@ fn play (jets: Vec<Move>, turns: usize) -> usize {
         };
         let mut shift = 0;
         let mut insert = chamber.len()+3;
+        //let mut tetra: usize = 0; // avoid copyright
+
         for _j in 0..7 { // load blank space above current sculpture
             chamber.push(0);
         }
@@ -79,12 +86,53 @@ fn play (jets: Vec<Move>, turns: usize) -> usize {
         let print_rock = place_rock(&rock, shift);
         for i in 0..4 {
             chamber[insert+i] = chamber[insert+i] | print_rock[i];
+            //if chamber[insert+i] == 0xfe {
+            //    tetra = insert+i;
+            //}
         }
         // Clear blank lines from top
         while chamber.last().unwrap() == &0 { chamber.pop();}
-        //print_map(chamber.clone());
+        // Clear unreachable lines from bottom
+        /*
+        if tetra > 0 {
+            let (_drop, keep) = chamber.split_at(tetra);
+            chamber = keep.to_vec();
+            height += tetra as u64;
+        }
+        */
+        // Check for a cycle
+        if turn%5 == 0 && turn > 10000 {
+            let slice = chamber
+                .clone()
+                .iter()
+                .rev()
+                .take(40)
+                .map(|s| *s as char)
+                .collect::<String>();
+            let visited = cycles
+                .insert((jet, slice.clone()), (turn, chamber.len() as u64 -1, 0));
+            match visited {
+                Some((old_turn, old_height, count)) => {
+                    let cycle_turns = turn - old_turn;
+                    let cycle_height = chamber.len() as u64 + height - old_height -1;
+                    let cycle_count = (turns -turn) / cycle_turns;
+                    if count > 1 {
+                        //println!("Found cycle at {}:{} and chamber height {} using ({},{}), turns: {} height: {} need {} to reach goal",
+                        //    old_turn, turn, chamber.len() -1 ,jet, slice.len(), cycle_turns, cycle_height, cycle_count);
+
+                        turn += cycle_turns * cycle_count;
+                        height += cycle_height * cycle_count;
+                        cycles.clear();
+                    } else {
+                        cycles.insert((jet,slice.clone()), (turn, chamber.len() as u64 -1, count+1));
+                    }
+                },
+                None => (),
+            }
+        }
+        turn += 1;
     }
-    chamber.len() - 1
+    chamber.len() as u64 - 1 + height
 }
 
 fn try_move (rock: &Rock, shift: i32, mov: Move, chamber: &[u8]) -> bool {
